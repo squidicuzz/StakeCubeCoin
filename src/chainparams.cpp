34afd101719ddda4262387d004b66d816f128d03
Liquid <Liquid369@gmail.com>
2022-09-15 07:57:30 -0500

    Set timestamp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -200,7 +200,7 @@ public:
         consensus.nMinerConfirmationWindow       = 2016; // nPowTargetTimespan / nPowTargetSpacing
 
         /** Prog PoW **/
-        consensus.nPPSwitchTime = INT_MAX; //set for later
+        consensus.nPPSwitchTime = 1663246600; //Thursday, September 15, 2022 12:56:40 PM GMT
         consensus.nInitialPPDifficulty = 0x1d016e81;    // 10MH/s
 
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit        = 28;
