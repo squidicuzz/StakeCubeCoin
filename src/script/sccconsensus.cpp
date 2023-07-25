0c99520839d6c90934b7894fac75e62b6e2d64bc
Jon <squid@sqdmc.net>
2023-07-24 20:49:24 -0400

    update consensus

--- src/script/sccconsensus.cpp
+++ src/script/sccconsensus.cpp
@@ -3,7 +3,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <script/sccconsensus.h>
+#include <script/bitcoinconsensus.h>
 
 #include <primitives/transaction.h>
 #include <pubkey.h>
@@ -52,7 +52,7 @@ private:
     size_t m_remaining;
 };
 
-inline int set_error(sccconsensus_error* ret, sccconsensus_error serror)
+inline int set_error(dashconsensus_error* ret, dashconsensus_error serror)
 {
     if (ret)
         *ret = serror;
@@ -70,36 +70,36 @@ ECCryptoClosure instance_of_eccryptoclosure;
 /** Check that all specified flags are part of the libconsensus interface. */
 static bool verify_flags(unsigned int flags)
 {
-    return (flags & ~(sccconsensus_SCRIPT_FLAGS_VERIFY_ALL)) == 0;
+    return (flags & ~(dashconsensus_SCRIPT_FLAGS_VERIFY_ALL)) == 0;
 }
 
-int sccconsensus_verify_script(const unsigned char *scriptPubKey, unsigned int scriptPubKeyLen,
+int dashconsensus_verify_script(const unsigned char *scriptPubKey, unsigned int scriptPubKeyLen,
                                     const unsigned char *txTo        , unsigned int txToLen,
-                                    unsigned int nIn, unsigned int flags, sccconsensus_error* err)
+                                    unsigned int nIn, unsigned int flags, dashconsensus_error* err)
 {
     if (!verify_flags(flags)) {
-        return set_error(err, sccconsensus_ERR_INVALID_FLAGS);
+        return set_error(err, dashconsensus_ERR_INVALID_FLAGS);
     }
     try {
         TxInputStream stream(PROTOCOL_VERSION, txTo, txToLen);
         CTransaction tx(deserialize, stream);
         if (nIn >= tx.vin.size())
-            return set_error(err, sccconsensus_ERR_TX_INDEX);
-        if (GetSerializeSize(tx, SER_NETWORK, PROTOCOL_VERSION) != txToLen)
-            return set_error(err, sccconsensus_ERR_TX_SIZE_MISMATCH);
+            return set_error(err, dashconsensus_ERR_TX_INDEX);
+        if (GetSerializeSize(tx, PROTOCOL_VERSION) != txToLen)
+            return set_error(err, dashconsensus_ERR_TX_SIZE_MISMATCH);
 
         // Regardless of the verification result, the tx did not error.
-        set_error(err, sccconsensus_ERR_OK);
+        set_error(err, dashconsensus_ERR_OK);
 
         PrecomputedTransactionData txdata(tx);
 		CAmount am(0);
         return VerifyScript(tx.vin[nIn].scriptSig, CScript(scriptPubKey, scriptPubKey + scriptPubKeyLen), flags, TransactionSignatureChecker(&tx, nIn, am, txdata), nullptr);
     } catch (const std::exception&) {
-        return set_error(err, sccconsensus_ERR_TX_DESERIALIZE); // Error deserializing
+        return set_error(err, dashconsensus_ERR_TX_DESERIALIZE); // Error deserializing
     }
 }
 
-unsigned int sccconsensus_version()
+unsigned int dashconsensus_version()
 {
     // Just use the API version for now
     return BITCOINCONSENSUS_API_VER;
