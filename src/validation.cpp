f729d26be10b19c4ac923f864197c395d6c58431
Jon <squid@sqdmc.net>
2023-08-17 06:44:52 -0400

    adjust mix_hash debug check

--- src/validation.cpp
+++ src/validation.cpp
@@ -1962,21 +1962,25 @@ bool CChainState::ConnectBlock(const CBlock& block, CValidationState& state, CBl
         LogPrintf("[DEBUG] block.mix_hash: %s\n", block.mix_hash.ToString());
         LogPrintf("[DEBUG] final_hash: %s\n", final_hash.ToString());
         LogPrintf("[DEBUG] block.hash: %s\n", block.GetHash().ToString());
-        if (block.nHeight > tip_nHeight){
+        bool bInvalidMix = false;
+        if (block.nHeight > tip_nHeight) {
             LogPrintf("[DEBUG] block.nHeight > tip_nHeight\n");
-            LogPrintf("[DEBUG] CChainState::ConnectBlock: This block doesn't exist yet?\n");
+            LogPrintf("[DEBUG] CChainState::ConnectBlock: This block doesn't exist yet!\n");
             // performs check for valid mix_hash.
-            if (exp_mix_hash != block.mix_hash) {
+            if (exp_mix_hash != block.mix_hash && !fJustCheck) {
+                bValidMix = true;
                 LogPrintf("[DEBUG][WARN] CChainState::ConnectBlock: Accepted result as 'REJECT_INVALID' with 'invalid-mixhash' -- 'mix_hash validity failed' :: exp_mix_hash != block.mix_hash :: %s != %s\n", exp_mix_hash.ToString(), block.mix_hash.ToString());
                 //return state.DoS(50, false, REJECT_INVALID, "invalid-mixhash", false, "mix_hash validity failed"); // warn invalid block???
+            } else {
+                LogPrintf("[DEBUG][OK] This block hasn't been mined yet!\n");
             }
         } else if (exp_mix_hash != block.mix_hash) {
             LogPrintf("[DEBUG][ERROR] CChainState::ConnectBlock: Accepted result as 'REJECT_INVALID' with 'invalid-mixhash' -- 'mix_hash validity failed' :: exp_mix_hash != block.mix_hash :: %s != %s\n", exp_mix_hash.ToString(), block.mix_hash.ToString());
-            //return state.DoS(50, false, REJECT_INVALID, "invalid-mixhash", false, "mix_hash validity failed"); // reject invalid block
+            return state.DoS(50, false, REJECT_INVALID, "invalid-mixhash", false, "mix_hash validity failed"); // reject invalid block
         } else {
             LogPrintf("[DEBUG][OK] block.nHeight <= tip_nHeight -- 'mix_hash validity passed'\n");
         }
-        if (!CheckProofOfWork(final_hash, block.nBits, chainparams.GetConsensus())) {
+        if (bInvalidMix && !CheckProofOfWork(final_hash, block.nBits, chainparams.GetConsensus())) {
             LogPrintf("[DEBUG][ERROR] CheckProofOfWork: FAILED -- Invalid final_hash: %s\n", final_hash.ToString());
         }
     }
